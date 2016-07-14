d84a9a56f0ac9004a3f1b050743e235738106f72
Henry <henry.li03@sap.com>
2016-07-14 10:31:47 +0800

    Add EventEmitter

--- src/nju/base/ManagedObject.js
+++ src/nju/base/ManagedObject.js
@@ -1,9 +1,12 @@
+import EventEmitter from "wolfy87-eventemitter";
+
 export default class ManagedObject
 {
     constructor(id = null)
     {
         this._id = id;
         this._parent = null;
+        this._eventEmitter = new EventEmitter();
         this.init();
     }
 
@@ -20,5 +23,32 @@ export default class ManagedObject
     get parent()
     {
         return this._parent;
+    }
+
+    get eventEmitter()
+    {
+        return this._eventEmitter;
+    }
+
+
+    on(type, listener)
+    {
+        this.eventEmitter.on(type, listener);
+        return this;
+    }
+
+    off(type, listener)
+    {
+        this.eventEmitter.off(type, listener);
+        return this;
+    }
+
+    trigger(type, parameters = {})
+    {
+        const event = {
+            type,
+            parameters
+        };
+        this.eventEmitter.trigger(type, [ event ]);
     }
 }
