b57c56e2c19a56046c232dfd87e3ebef8b6bc58a
Henry <henry.li03@sap.com>
2016-07-14 12:48:12 +0800

    Add ViewController and ApplicationController

---
+++ src/nju/app/ApplicationController.js
@@ -0,0 +1,10 @@
+import Application from "./Application";
+import ViewController from "../view/ViewController";
+
+export default class ApplicationController extends ViewController
+{
+    run()
+    {
+
+    }
+}
