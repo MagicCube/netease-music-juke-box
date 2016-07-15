cc8764a0baa0f3cf3bcfa5507d61d0bad9ac7bf1
Henry <henry.li03@sap.com>
2016-07-15 10:44:11 +0800

    fix

--- src/nm/view/PlayerView.js
+++ src/nm/view/PlayerView.js
@@ -6,5 +6,31 @@ export default class PlayerView extends View
     {
         super.init();
         this.addStyleClass("nm-player-view");
+
+        this._track = null;
+
+        this._initLayout();
+    }
+
+    get track()
+    {
+        return this._track;
+    }
+    set track(value)
+    {
+        this._track = value;
+        if (value)
+        {
+            this.$element.find(".name").text(value.name);
+        }
+        else
+        {
+            this.$element.find(".name").text("");
+        }
+    }
+
+    _initLayout()
+    {
+        this.$element.append(`<span class="name"></span>`);
     }
 }
